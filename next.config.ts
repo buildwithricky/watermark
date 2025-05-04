1acff6d26c44f842e610b31591bf3b5174766f7b
patrick <patokezy@gmail.com>
2025-05-04 22:49:00 +0100

    testing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ⚠️ This disables ESLint checks during builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, // disables type checking during build
+  },
 };
 
 export default nextConfig;
